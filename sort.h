f162a4333e78d48c136e2cc882f7a930ca1ed747
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-25 11:31:23 +0000

    quick sort

--- sort.h
+++ sort.h
@@ -23,4 +23,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int quick_sort_and_partition(int *array, int low, int high);
+void quickSortRecursive(int *array, int low, int high);
 #endif
