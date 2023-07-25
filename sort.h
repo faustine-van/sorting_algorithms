b3017be749e76c954a8c9d0fa21237507675d24c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-25 19:25:52 +0000

    uodated

--- sort.h
+++ sort.h
@@ -23,8 +23,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *a, int *b);
-int lomuto_partition(int *array, int low, int high, size_t size);
-void quickSortRecursive(int *array, int low, int high, size_t size);
-
+void lomuto_partition(int *array, int low, int high, size_t size);
 #endif
